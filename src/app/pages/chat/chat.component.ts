cd97e0aa1c340a88ac9effec938a69b068bb60e9
Rod97139 <fayaflame@gmail.com>
2025-03-10 23:01:34 +0100

    install talkjs

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Talk from 'talkjs';
 
 @Component({
   selector: 'app-chat',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+  constructor() {
+    Talk.ready.then((): void => {
+      const me = new Talk.User('sample_user_alice');
+      const session = new Talk.Session({
+        appId: '<APP_ID>',
+        me: me,
+      });
+
+      const conversation = session.getOrCreateConversation(
+        'sample_conversation'
+      );
+      conversation.setParticipant(me);
+
+      const chatbox = session.createChatbox();
+      chatbox.select(conversation);
+      chatbox.mount(document.getElementById('talkjs-container'));
+    });
+  }
 
 }
